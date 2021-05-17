88675a0a6c4d648308e46ac5dabee4a9539a6fd7
nielinjie <nielinjie@gmail.com>
2021-05-17 20:58:14 +0800

    update

--- src/modelLogs.ts
+++ src/modelLogs.ts
@@ -4,7 +4,8 @@ import { log } from "./Util";
 export async function modelLogs(): Promise<void> {
   return logs.then(async (ls) => {
     ls.forEach((l) => {
-      if (l.level === "error") log.error(l.message);
+      if (l.level === "error") log.error(l.message,' - ',l.context);
+      else if (l.level === "warning") log.warning(l.message,' - ',l.context);
       else {
         log.info(l.message);
       }
