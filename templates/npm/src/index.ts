c54b140006fd63d1033f936d0ab80cf8a65640de
nielinjie <nielinjie@gmail.com>
2021-07-18 09:20:34 +0800

    template update

--- templates/npm/src/index.ts
+++ templates/npm/src/index.ts
@@ -3,6 +3,6 @@ env.config();
 
 import launcherImplementation from "../launcherImplementation.json";
 import { launch } from "@quick-qui/launcher";
-import { Implementation } from "@quick-qui/model-defines";
+import { Implementation } from "@quick-qui/implementation-model";
 
 launch(launcherImplementation as Implementation);
